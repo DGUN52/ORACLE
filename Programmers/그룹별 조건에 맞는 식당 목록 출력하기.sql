94e04e27f5d3bc1a0108d0060121a726509196d2
D__G_UN <eorms96@naver.com>
2023-09-18 22:33:07 +0900

    Create 그룹별 조건에 맞는 식당 목록 출력하기.sql

---
+++ Programmers/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -0,0 +1,24 @@
+SELECT 
+    P.MEMBER_NAME
+    , R.REVIEW_TEXT
+    , TO_CHAR(R.REVIEW_DATE,'yyyy-mm-dd') REVIEW_DATE
+FROM
+    MEMBER_PROFILE P
+    , REST_REVIEW R
+    , (
+        SELECT MEMBER_ID
+        FROM (
+            SELECT MEMBER_ID, COUNT(*)
+            FROM REST_REVIEW
+            GROUP BY MEMBER_ID
+            ORDER BY 2 DESC
+            )
+        WHERE ROWNUM<=1
+    ) MOST
+WHERE 
+    P.MEMBER_ID = R.MEMBER_ID
+    AND P.MEMBER_ID = MOST.MEMBER_ID
+ORDER BY
+    3 ASC,
+    2 ASC
+;

32549497e9774ef5d9c26cc4b0329826b7ddc99e
D__G_UN <eorms96@naver.com>
2023-09-18 23:42:15 +0900

    Update 그룹별 조건에 맞는 식당 목록 출력하기.sql

--- Programmers/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ Programmers/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,27 +1,27 @@
-SELECT 
-    P.MEMBER_NAME
-    , R.REVIEW_TEXT
-    , TO_CHAR(R.REVIEW_DATE,'yyyy-mm-dd') REVIEW_DATE
-FROM
-    MEMBER_PROFILE P
-    , REST_REVIEW R
-    , (
-        SELECT MEMBER_ID
-        FROM (
-            SELECT MEMBER_ID, COUNT(*)
-            FROM REST_REVIEW
-            GROUP BY MEMBER_ID
-            ORDER BY 2 DESC
-            )
-        WHERE ROWNUM<=1
-    ) MOST
-WHERE 
-    P.MEMBER_ID = R.MEMBER_ID
-    AND P.MEMBER_ID = MOST.MEMBER_ID
-ORDER BY
-    3 ASC,
-    2 ASC
-;
+-- SELECT 
+--     P.MEMBER_NAME
+--     , R.REVIEW_TEXT
+--     , TO_CHAR(R.REVIEW_DATE,'yyyy-mm-dd') REVIEW_DATE
+-- FROM
+--     MEMBER_PROFILE P
+--     , REST_REVIEW R
+--     , (
+--         SELECT MEMBER_ID
+--         FROM (
+--             SELECT MEMBER_ID, COUNT(*)
+--             FROM REST_REVIEW
+--             GROUP BY MEMBER_ID
+--             ORDER BY 2 DESC
+--             )
+--         WHERE ROWNUM<=1
+--     ) MOST
+-- WHERE 
+--     P.MEMBER_ID = R.MEMBER_ID
+--     AND P.MEMBER_ID = MOST.MEMBER_ID
+-- ORDER BY
+--     3 ASC,
+--     2 ASC
+-- ;
 
 /*
 SELECT 
@@ -48,3 +48,27 @@ ORDER BY
     2 ASC
 ;
 */
+
+-- Oracle
+SELECT 
+    P.MEMBER_NAME
+    , R.REVIEW_TEXT
+    , TO_CHAR(R.REVIEW_DATE,'yyyy-mm-dd') REVIEW_DATE
+FROM
+    MEMBER_PROFILE P
+    , REST_REVIEW R
+WHERE 
+    P.MEMBER_ID = R.MEMBER_ID
+    AND P.MEMBER_ID IN (
+        SELECT MEMBER_ID
+        FROM (
+            SELECT MEMBER_ID, RANK() OVER(ORDER BY COUNT(*) DESC) rnk
+            FROM REST_REVIEW
+            GROUP BY MEMBER_ID
+            )
+        WHERE rnk=1
+    )
+ORDER BY
+    3 ASC,
+    2 ASC
+;
